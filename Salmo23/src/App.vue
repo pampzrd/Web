0de193e2f211181709b07b59945e51ca5b5bf51f
Pamela Rodrigues <akahime2014@gmail.com>
2025-05-30 18:03:26 -0300

    Include Vue projects to WIP branch

---
+++ Salmo23/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<main-app/>
+</template>
+
+
+<script>
+import MainApp from "@/components/main-app.vue";
+
+export default {
+  name: 'App',
+  components: {
+    MainApp,
+
+  }
+}
+</script>
+
+<style>
+
+#App {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+
+}
+</style>
---
+++ Salmo23/src/components/titulo-principal.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>SALMO 23</h1>
+</template>
+
+<style scoped>
+h1{
+  font-family: "Cal Sans", sans-serif;
+  color: #ffffff;
+  text-shadow: 3px 6px rgba(55, 138, 40, 0.25);
+  text-align: center;
+  font-size: 102pt;
+
+}
+
+
+</style>
